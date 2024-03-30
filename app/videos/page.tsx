f68fd734382db4f3fd10e0e6bec32598fc83adde
Jason Zhang <zjjc123@hotmail.com>
2024-03-29 23:28:16 -0700

    Add Video

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -8,17 +8,21 @@ export default function VideoPage({}: Props) {
     <div>
       <div className="flex flex-col justify-center my-8">
         <VideoModule
-          title="EUPHORIA X Bollywood - UW Kahaani"
-          src="PMSljieUlmE"
+          title="University of Washington ASUW Black Student Commission"
+          src="Q0rLdrsbAvU"
         />
         <VideoModule
-          title="Ude Dil Levitating - UW Kahaani"
+          title="Ude Dil Levitating - UW Kahaani (1.5M+ Views)"
           src="kxFhZwQ3eu0"
         />
         <VideoModule title="VEX Worlds Reveal - 917M" src="lWE1iFQBnmM" />
         <VideoModule
           title="Viral Vision | Breakthrough Junior Challenge"
           src="8PgU2d7qOgM"
+        />
+        <VideoModule
+          title="EUPHORIA X Bollywood - UW Kahaani"
+          src="PMSljieUlmE"
         />
         <VideoModule title="PSA 2020 ILC Winner - HOSA" src="iL9gJoI2TR4" />
       </div>
