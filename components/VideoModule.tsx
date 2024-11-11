afd5c4e03528edcca9bb117d5589f7fbed7b4e71
Jason Zhang <zjjc123@hotmail.com>
2024-11-11 00:08:01 -0800

    feat: skeleton on iframe load

--- components/VideoModule.tsx
+++ components/VideoModule.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { Card, Title } from '@mantine/core';
+import { Card, Skeleton, Title } from '@mantine/core';
 
 interface VideoModuleProps {
   title: string;
@@ -9,25 +9,39 @@ interface VideoModuleProps {
 }
 
 export default function VideoModule({ title, src }: VideoModuleProps) {
+  const [loading, setLoading] = React.useState(true);
+
   return (
     <Card shadow="sm" padding="lg" radius="md" withBorder>
       <Title order={2} mb="md">
         {title}
       </Title>
-      <div style={{ position: 'relative', paddingTop: '56.25%' }}>
-        <iframe
-          title={title}
-          src={'https://youtube.com/embed/' + src}
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
+      <div style={{ position: 'relative', paddingTop: '40%' }}>
+        <Skeleton
+          visible={loading}
           style={{
+            height: '100%',
             position: 'absolute',
             top: 0,
             left: 0,
-            width: '100%',
-            height: '100%',
+            right: 0,
           }}
-        />
+        >
+          <iframe
+            title={title}
+            src={'https://youtube.com/embed/' + src}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+            }}
+            onLoad={() => setLoading(false)}
+          />
+        </Skeleton>
       </div>
     </Card>
   );
